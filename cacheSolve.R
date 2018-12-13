03bda0cdab392e3fa855a01062e3f3e62aca300f
Robert Smillie <robertjsmillie94@gmail.com>
2018-12-13 17:57:16 +0000

    adding comments on code

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,12 +1,12 @@
 cacheSolve <- function(x, ...) {
   library(MASS)
-  m <- x$getinverse()
-  if(!is.null(m)) {
+  m <- x$getinverse() #Get the cached inverse variable, m
+  if(!is.null(m)) { #if m is NOT null, return m
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- ginv(data, ...)
-  x$setinverse(m)
-  m
+  data <- x$get() #if not, get the data for x
+  m <- ginv(data, ...) #get the inverse of the data
+  x$setinverse(m) #cache the inverse m
+  m #return the inverse m
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,14 +1,14 @@
 makeCacheMatrix <- function(x = matrix()) {
   library(MASS)
-  m <- NULL
-  set <- function(y) {
+  m <- NULL #set m the inverse variable to be NULL
+  set <- function(y) { #Define the set function
     x <<- y
     m <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
-  list(set = set, get = get,
+  get <- function() x #Define the get function
+  setinverse <- function(inverse) m <<- inverse #Define the setinverse function
+  getinverse <- function() m #Define the getinverse function
+  list(set = set, get = get, #Return the list containing the 4 functions
        setinverse = setinverse,
        getinverse = getinverse)
 }
