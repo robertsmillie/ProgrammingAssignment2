10e9d055b319df6c6297336464e2baf6558163ed
Robert Smillie <robertjsmillie94@gmail.com>
2018-12-13 17:37:43 +0000

    Adding R files

---
+++ cacheSolve.R
@@ -0,0 +1,12 @@
+cacheSolve <- function(x, ...) {
+  library(MASS)
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setinverse(m)
+  m
+}
